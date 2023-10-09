803a08e531a8852c48723085a8a9113cee21422b
LAPTOP-71J3QU54\Acer <hieu.nguyen22012001@hcmut.edu.vn>
2023-10-09 20:13:59 +0700

    no message

--- lab2_exercise/LAB2_Exercise/Core/Src/main.c
+++ lab2_exercise/LAB2_Exercise/Core/Src/main.c
@@ -134,36 +134,21 @@ void HAL_TIM_PeriodElapsedCallback (TIM_HandleTypeDef *htim2 )
   */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
-  /* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_TIM2_Init();
-  /* USER CODE BEGIN 2 */
   HAL_TIM_Base_Start_IT (& htim2 );
-  /* USER CODE END 2 */
+
   void SCAN (int num){
    switch(num){
    case 0:
-  	 // set Status
+
   		HAL_GPIO_WritePin(SEG0_GPIO_Port, SEG0_Pin, 0 );
   		HAL_GPIO_WritePin(SEG1_GPIO_Port,SEG1_Pin,  0);
   		HAL_GPIO_WritePin(SEG2_GPIO_Port, SEG2_Pin, 0);
